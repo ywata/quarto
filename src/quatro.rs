ce29a0a9302b798f9f13657e6f362b7300932a24
Yasu Watanabe <ywata@protonmail.com>
2024-05-13 10:58:02 +0900

    refactor: by pre-commit

--- src/quatro.rs
+++ src/quatro.rs
@@ -1,10 +1,9 @@
-
 use std::collections::HashMap;
 use std::hash::Hash;
 
+use serde::{Deserialize, Serialize};
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
-use serde::{Serialize, Deserialize};
 
 #[derive(Debug)]
 enum QuatroIoError {
@@ -42,8 +41,6 @@ impl TryFrom<&str> for Color {
     }
 }
 
-
-
 #[derive(Clone, Debug, EnumIter, Eq, Hash, Deserialize, Serialize, PartialEq)]
 enum Height {
     Short,
@@ -76,7 +73,6 @@ enum Shape {
     Square,
 }
 
-
 impl From<Shape> for String {
     fn from(s: Shape) -> Self {
         match s {
@@ -96,8 +92,6 @@ impl TryFrom<&str> for Shape {
         }
     }
 }
-
-
 
 #[derive(Clone, Debug, EnumIter, Eq, Hash, Deserialize, Serialize, PartialEq)]
 enum Top {
@@ -125,8 +119,6 @@ impl TryFrom<&str> for Top {
     }
 }
 
-
-
 #[derive(Clone, Debug, Deserialize, Eq, Hash, Serialize, PartialEq)]
 struct Piece {
     color: Color,
@@ -135,14 +127,16 @@ struct Piece {
     top: Top,
 }
 
-
 impl From<Piece> for String {
     fn from(p: Piece) -> Self {
-        return format!("{}{}{}{}",
-                       Self::from(p.color),
-                       Self::from(p.height),
-                       Self::from(p.shape),
-                       Self::from(p.top)).to_string();
+        return format!(
+            "{}{}{}{}",
+            Self::from(p.color),
+            Self::from(p.height),
+            Self::from(p.shape),
+            Self::from(p.top)
+        )
+        .to_string();
     }
 }
 
@@ -150,7 +144,7 @@ impl TryFrom<String> for Piece {
     type Error = QuatroIoError;
     fn try_from(text: String) -> Result<Piece, Self::Error> {
         if text.len() != 4 {
-            return Err(QuatroIoError::InvalidPieceError)
+            return Err(QuatroIoError::InvalidPieceError);
         }
         let color = &text[0..1];
         let height = &text[1..2];
@@ -166,9 +160,6 @@ impl TryFrom<String> for Piece {
     }
 }
 
-
-
-
 /* Nothing corresponded to empty cell */
 type CellState = Option<Piece>;
 
@@ -179,14 +170,18 @@ struct Board {
     available_pieces: Vec<Piece>,
 }
 
-
-fn all_pieces() -> Vec<Piece>{
+fn all_pieces() -> Vec<Piece> {
     let mut pieces: Vec<Piece> = Vec::new();
     for c in Color::iter() {
         for s in Shape::iter() {
             for t in Top::iter() {
                 for h in Height::iter() {
-                    pieces.push(Piece {color:c.clone(), shape:s.clone(), top:t.clone(), height:h.clone()});
+                    pieces.push(Piece {
+                        color: c.clone(),
+                        shape: s.clone(),
+                        top: t.clone(),
+                        height: h.clone(),
+                    });
                 }
             }
         }
@@ -194,27 +189,29 @@ fn all_pieces() -> Vec<Piece>{
     pieces
 }
 
-
 impl Board {
-
     pub fn new() -> Self {
         Board {
             board: vec![vec![CellState::None; 4]; 4],
             available_pieces: all_pieces(),
         }
     }
-    fn count_elements<S: Clone + Eq + PartialEq + Hash>(&self, coords: &Vec<(usize, usize)>, prop: fn(Piece)->S) -> (bool, usize) {
+    fn count_elements<S: Clone + Eq + PartialEq + Hash>(
+        &self,
+        coords: &Vec<(usize, usize)>,
+        prop: fn(Piece) -> S,
+    ) -> (bool, usize) {
         let picked: Vec<_> = coords
             .into_iter()
             .map(|(x, y)| self.board[*x][*y].clone())
             .collect();
-        let picked_property : Vec<Option<S>>= picked
+        let picked_property: Vec<Option<S>> = picked
             .clone()
             .iter()
             .map(|opt| opt.as_ref().map(|p| prop(p.clone())))
             .collect();
 
-        let mut hmap : HashMap<Option<S>, usize> = HashMap::new();
+        let mut hmap: HashMap<Option<S>, usize> = HashMap::new();
         let mut found_none = false;
         for v in picked_property {
             if let None = v {
@@ -229,22 +226,35 @@ impl Board {
         (found_none, hmap.len())
     }
 
-
-    pub fn parse_quatro(self, coords_vec: Vec<Vec<(usize, usize)>>) ->  Vec<(Vec<(usize, usize)>, ((bool, usize), (bool, usize), (bool, usize), (bool, usize)))> {
-        let mut ret: Vec<(Vec<(usize, usize)>, ((bool, usize), (bool, usize), (bool, usize), (bool, usize)))> = Vec::new();
+    pub fn parse_quatro(
+        self,
+        coords_vec: Vec<Vec<(usize, usize)>>,
+    ) -> Vec<(
+        Vec<(usize, usize)>,
+        ((bool, usize), (bool, usize), (bool, usize), (bool, usize)),
+    )> {
+        let mut ret: Vec<(
+            Vec<(usize, usize)>,
+            ((bool, usize), (bool, usize), (bool, usize), (bool, usize)),
+        )> = Vec::new();
         for coords in coords_vec {
             let color_count = &self.count_elements(&coords, |piece| piece.color);
             let shape_count = &self.count_elements(&coords, |piece| piece.shape);
             let height_count = &self.count_elements(&coords, |piece| piece.height);
             let top_count = &self.count_elements(&coords, |piece| piece.top);
-            let quatro = (color_count.clone(), shape_count.clone(), height_count.clone(), top_count.clone());
+            let quatro = (
+                color_count.clone(),
+                shape_count.clone(),
+                height_count.clone(),
+                top_count.clone(),
+            );
             ret.push((coords.clone(), quatro));
         }
 
         ret
     }
 
-    pub fn parse_board_text(text: &String) -> Option<Board>{
+    pub fn parse_board_text(text: &String) -> Option<Board> {
         let mut game = Board::new();
         let lines: Vec<&str> = text.lines().collect();
         if lines.len() != 4 {
@@ -252,15 +262,15 @@ impl Board {
         }
         let mut x: usize = 0;
         for line in lines {
-            if line.len() != 3 *(4+1) + 4{
-                return None
+            if line.len() != 3 * (4 + 1) + 4 {
+                return None;
             }
 
             for y in 0..4 {
-                let piece_text = &line[5 * y..5* y +4];
+                let piece_text = &line[5 * y..5 * y + 4];
                 if piece_text.eq("    ") {
                     game.board[x][y] = None;
-                }else {
+                } else {
                     let piece = Piece::try_from(piece_text.to_string()).ok()?;
                     game.board[x][y] = Some(piece);
                 }
@@ -274,20 +284,16 @@ impl Board {
             }
 
             x += 1;
-
         }
         Some(game)
     }
-
 }
-
-
 
 #[cfg(test)]
 mod test {
+    use super::*;
     use env_logger::fmt::style::AnsiColor::Black;
     use indoc::indoc;
-    use super::*;
     #[test]
     fn test_board_new() {
         let board = Board::new();
@@ -303,7 +309,7 @@ mod test {
     fn test_parse_all_pieces() {
         /* WB TS SC HF */
         let board_text = indoc! {
-            r#"BSCF BSCH BSSF BSSH
+        r#"BSCF BSCH BSSF BSSH
                BTCF BTCH BTSF BTSH
                WSCF WSCH WSSF WSSH
                WTCF WTCH WTSF WTSH
@@ -313,41 +319,117 @@ mod test {
 
         let expected = vec![
             vec![
-                Some(Piece{color: Color::Brown, height:Height::Short, shape: Shape::Circle, top: Top::Flat,}),
-                Some(Piece{color: Color::Brown, height:Height::Short, shape: Shape::Circle, top: Top::Hole,}),
-                Some(Piece{color: Color::Brown, height:Height::Short, shape: Shape::Square, top: Top::Flat,}),
-                Some(Piece{color: Color::Brown, height:Height::Short, shape: Shape::Square, top: Top::Hole,}),
+                Some(Piece {
+                    color: Color::Brown,
+                    height: Height::Short,
+                    shape: Shape::Circle,
+                    top: Top::Flat,
+                }),
+                Some(Piece {
+                    color: Color::Brown,
+                    height: Height::Short,
+                    shape: Shape::Circle,
+                    top: Top::Hole,
+                }),
+                Some(Piece {
+                    color: Color::Brown,
+                    height: Height::Short,
+                    shape: Shape::Square,
+                    top: Top::Flat,
+                }),
+                Some(Piece {
+                    color: Color::Brown,
+                    height: Height::Short,
+                    shape: Shape::Square,
+                    top: Top::Hole,
+                }),
             ],
             vec![
-                Some(Piece{color: Color::Brown, height:Height::Tall, shape: Shape::Circle, top: Top::Flat,}),
-                Some(Piece{color: Color::Brown, height:Height::Tall, shape: Shape::Circle, top: Top::Hole,}),
-                Some(Piece{color: Color::Brown, height:Height::Tall, shape: Shape::Square, top: Top::Flat,}),
-                Some(Piece{color: Color::Brown, height:Height::Tall, shape: Shape::Square, top: Top::Hole,}),
-
+                Some(Piece {
+                    color: Color::Brown,
+                    height: Height::Tall,
+                    shape: Shape::Circle,
+                    top: Top::Flat,
+                }),
+                Some(Piece {
+                    color: Color::Brown,
+                    height: Height::Tall,
+                    shape: Shape::Circle,
+                    top: Top::Hole,
+                }),
+                Some(Piece {
+                    color: Color::Brown,
+                    height: Height::Tall,
+                    shape: Shape::Square,
+                    top: Top::Flat,
+                }),
+                Some(Piece {
+                    color: Color::Brown,
+                    height: Height::Tall,
+                    shape: Shape::Square,
+                    top: Top::Hole,
+                }),
             ],
             vec![
-                Some(Piece{color: Color::White, height:Height::Short, shape: Shape::Circle, top: Top::Flat,}),
-                Some(Piece{color: Color::White, height:Height::Short, shape: Shape::Circle, top: Top::Hole,}),
-                Some(Piece{color: Color::White, height:Height::Short, shape: Shape::Square, top: Top::Flat,}),
-                Some(Piece{color: Color::White, height:Height::Short, shape: Shape::Square, top: Top::Hole,}),
-
+                Some(Piece {
+                    color: Color::White,
+                    height: Height::Short,
+                    shape: Shape::Circle,
+                    top: Top::Flat,
+                }),
+                Some(Piece {
+                    color: Color::White,
+                    height: Height::Short,
+                    shape: Shape::Circle,
+                    top: Top::Hole,
+                }),
+                Some(Piece {
+                    color: Color::White,
+                    height: Height::Short,
+                    shape: Shape::Square,
+                    top: Top::Flat,
+                }),
+                Some(Piece {
+                    color: Color::White,
+                    height: Height::Short,
+                    shape: Shape::Square,
+                    top: Top::Hole,
+                }),
             ],
             vec![
-                Some(Piece{color: Color::White, height:Height::Tall, shape: Shape::Circle, top: Top::Flat,}),
-                Some(Piece{color: Color::White, height:Height::Tall, shape: Shape::Circle, top: Top::Hole,}),
-                Some(Piece{color: Color::White, height:Height::Tall, shape: Shape::Square, top: Top::Flat,}),
-                Some(Piece{color: Color::White, height:Height::Tall, shape: Shape::Square, top: Top::Hole,}),
-
+                Some(Piece {
+                    color: Color::White,
+                    height: Height::Tall,
+                    shape: Shape::Circle,
+                    top: Top::Flat,
+                }),
+                Some(Piece {
+                    color: Color::White,
+                    height: Height::Tall,
+                    shape: Shape::Circle,
+                    top: Top::Hole,
+                }),
+                Some(Piece {
+                    color: Color::White,
+                    height: Height::Tall,
+                    shape: Shape::Square,
+                    top: Top::Flat,
+                }),
+                Some(Piece {
+                    color: Color::White,
+                    height: Height::Tall,
+                    shape: Shape::Square,
+                    top: Top::Hole,
+                }),
             ],
         ];
         assert_eq!(expected, board.unwrap().board)
-
     }
     #[test]
     fn test_empty_board() {
         let dummy_text = indoc! {
-            /* - will be replaced to space */
-            r#"
+        /* - will be replaced to space */
+        r#"
             BSCF ---- ---- ----
             ---- ---- ---- ----
             ---- ---- ---- ----
@@ -356,7 +438,17 @@ mod test {
 
         let board = Board::parse_board_text(&board_text.to_string());
         let expected = vec![
-            vec![Some(Piece{color: Color::Brown, height:Height::Short, shape: Shape::Circle, top: Top::Flat,}), None, None, None],
+            vec![
+                Some(Piece {
+                    color: Color::Brown,
+                    height: Height::Short,
+                    shape: Shape::Circle,
+                    top: Top::Flat,
+                }),
+                None,
+                None,
+                None,
+            ],
             vec![None, None, None, None],
             vec![None, None, None, None],
             vec![None, None, None, None],
@@ -366,7 +458,7 @@ mod test {
     #[test]
     fn test_judge_quatro() {
         let board_text = indoc! {
-            r#"BSCF WWSB BSSW WWSB
+        r#"BSCF WWSB BSSW WWSB
                BSSW WWSB BSSW WWSB
                BSSW WWSB BSSW WWSB
                BSSW WWSB BSSW WWSB
